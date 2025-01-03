085308de8c4471c01dc3111adbfb73bae62c6f05
manishmmj <164055745+manishmmj@users.noreply.github.com>
2025-01-03 19:07:48 +0530

    Update app1_anime_reccomend.py

--- app1_anime_reccomend.py
+++ app1_anime_reccomend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+import joblib
 
     
 # https://drive.google.com/file/d/12inLzbTbbKFGKXdCMZrZWb65HwWRExo6/view?usp=drive_link
