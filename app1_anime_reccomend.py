a72a9f36c192706e1bf8c9cc31d92a5c43e86c69
manishmmj <164055745+manishmmj@users.noreply.github.com>
2025-01-04 10:56:38 +0530

    Update app1_anime_reccomend.py

--- app1_anime_reccomend.py
+++ app1_anime_reccomend.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
     
 # https://drive.google.com/file/d/12inLzbTbbKFGKXdCMZrZWb65HwWRExo6/view?usp=drive_link
 
